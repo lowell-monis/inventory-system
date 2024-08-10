2018a49989289eae3ba6792dc186142ffea9c1cf
lowell-monis <monislow@msu.edu>
2024-08-10 00:14:34 -0400

    firestore configuration

---
+++ inventory_management/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDqbTJ96855Cmgvev2A1YnXPMN87dnmjNQ",
+  authDomain: "pantrynomenon.firebaseapp.com",
+  projectId: "pantrynomenon",
+  storageBucket: "pantrynomenon.appspot.com",
+  messagingSenderId: "788567667043",
+  appId: "1:788567667043:web:da0e16ebf4b1ae08eaa356",
+  measurementId: "G-PTSV03M1E8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore };
